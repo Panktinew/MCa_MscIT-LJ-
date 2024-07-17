0ba9711662842d57273ec2cf92bc3d0d4b0979d2
Panktinew <130337976+Panktinew@users.noreply.github.com>
2024-07-17 12:46:14 +0530

    Add files via upload

---
+++ firstproject/src/firstproject/FirstClass.java
@@ -0,0 +1,59 @@
+
+/* take percentage from user
+ * if per > 90  then print grade A
+ * if between 80 to 90 then grade b
+ * if less than 80 grade c
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+
+package firstproject;
+
+import java.util.Scanner;
+
+public class FirstClass {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		
+		/*int no1,no2;
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter 2 numbers");
+		no1=  sc.nextInt();
+		no2=sc.nextInt();
+		float ans =  (float) no1/no2;
+		System.out.println(ans);
+		
+		if(no1 >90) {
+			
+		}
+		
+		if(no1>80 && no1<90) {
+			
+		}
+		if( no1<80) {
+			
+		}
+		
+		
+		*/
+		
+		
+		
+
+  for(int k=11,i=1 ;  k>0 && i<=10  ;  k--,i++) {
+	  System.out.println(k+ "  " + i);
+	   
+  }
+		
+		
+		
+	}
+
+}
